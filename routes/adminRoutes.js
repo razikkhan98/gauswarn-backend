cf543d1cc3aa0978343639c4c1a1d871a9105af4
Usman <usman.hasan619@gmail.com>
2025-01-08 19:00:55 +0530

    fix

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -4,12 +4,18 @@ const express = require("express");
 const productController = require("../controllers/productController");
 const router = express.Router();
 
-/**
- *
- * Products routes
- *
- * */
-
+/** Products routes  * */
+// create
 router.post("/createProduct", productController.addProduct);
+// get All
+router.get("/getAllProduct", productController.getAllProducts);
+// get single by id
+router.post("/getProductById/:id", productController.getProductById);
+// update single by id
+router.post("/updateProductById/:id", productController.updateProduct);
+// delete
+router.post("/deleteProductById/:id", productController.deleteProduct);
+
+/** Products routes end * */
 
 module.exports = router;
